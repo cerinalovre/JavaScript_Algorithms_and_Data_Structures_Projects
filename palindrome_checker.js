fedbc75639a2b8e1a99c3b223c8ddac9f2098883
cerinalovre <83784333+cerinalovre@users.noreply.github.com>
2022-04-14 15:04:39 +0200

    made the palindrome checker

---
+++ palindrome_checker.js
@@ -0,0 +1,20 @@
+function isAlphaNum(char) {
+    return char.match(/[a-z]/i) || char.match(/[0-9]/);
+  }
+  
+function palindrome(str) {
+    str = str.toLowerCase();
+    let normalStr = '';
+    let reversedStr = '';
+  
+    for (let i = 0; i < str.length; i++) {
+        if (isAlphaNum(str[i])) {
+            normalStr = normalStr + str[i];
+            reversedStr = str[i] + reversedStr;
+      }
+    }
+    return normalStr === reversedStr;
+  }
+  
+console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(palindrome("not a palindrome"));
