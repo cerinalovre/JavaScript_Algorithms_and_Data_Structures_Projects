3025059d143890a8f55c565355da0478bbc5c0db
cerinalovre <83784333+cerinalovre@users.noreply.github.com>
2022-04-15 18:16:42 +0200

    made the roman numeral converter

---
+++ roman_converter.js
@@ -0,0 +1,31 @@
+function convertToRoman(num) {
+    const numerals = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+        10: 'X',
+        40: 'XL',
+        50: 'L',
+        90: 'XC',
+        100: 'C',
+        400: 'CD',
+        500: 'D',
+        900: 'CM',
+        1000: 'M'
+    };
+
+    let romanNum = '';
+    const decimalKeys = Object.keys(numerals).reverse();
+
+    decimalKeys.forEach(key => {
+        while (key <= num) {
+            romanNum += numerals[key];
+            num -= key;
+        }
+    });
+
+    return romanNum;
+}
+   
+console.log(convertToRoman(36));
